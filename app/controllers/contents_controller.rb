cb593a0053dcd2132081e9833bc771d04c6ba65e
melanie lepage <melanie.lepage86@hotmail.fr>
2025-03-18 12:55:14 +0100

    controller content => in progress

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,30 @@
 class ContentsController < ApplicationController
+before_action :set_concert
+before_action :set_content
+
+  def index
+    @concert = Concert.find(params[:concert_id])
+    @content = Content.all
+  end
+
+  def show
+    @concert = Concert.find(params[:concert_id])
+    @content = Content.new
+  end
+
+  def new
+    @concert = Concert.find(params[:concert_id])
+    @content = Content.new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_concert
+  end
+
+  def set_content
+  end
 end
