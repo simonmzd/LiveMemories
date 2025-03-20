42abdf5d910ac06d30019790c884d30cf09119a0
vincent testard <vincent.testard2003@gmail.com>
2025-03-20 17:31:50 +0100

    front dashboard ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#landing"
   get "auth", to: "pages#auth"
-  
-
-  resources :concerts, only: %i[index show new create] do
+  get "dashboard", to: "pages#dashboard"
+  resources :concerts do
     resources :contents do
       resources :votes, only: %i[create destroy update]
       resources :comments, only: %i[create destroy new update]
