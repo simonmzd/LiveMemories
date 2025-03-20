9eaead308593870ff791df3d9a7e2767b6de86b7
melanie lepage <melanie.lepage86@hotmail.fr>
2025-03-20 17:34:33 +0100

    Front show contents en cours

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#landing"
   get "auth", to: "pages#auth"
-  
+  get "dashboard", to: "pages#dashboard"
 
-  resources :concerts, only: %i[index show new create] do
+
+  resources :concerts do
     resources :contents do
       resources :votes, only: %i[create destroy update]
       resources :comments, only: %i[create destroy new update]
