396761853f827c8e6d1f7b5eb05fa6bef38bfcb7
melanie lepage <melanie.lepage86@hotmail.fr>
2025-03-21 18:17:51 +0100

    Front du vendredi aprem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  MOIS_FR = {
+    "Jan" => "Janv.", "Feb" => "Févr.", "Mar" => "Mars", "Apr" => "Avr.", "May" => "Mai", "Jun" => "Juin",
+    "Jul" => "Juil.", "Aug" => "Août", "Sep" => "Sept.", "Oct" => "Oct.", "Nov" => "Nov.", "Dec" => "Déc."
+  }.freeze
+
+  JOURS_FR = {
+    "Mon" => "Lun.", "Tue" => "Mar.", "Wed" => "Mer.", "Thu" => "Jeu.", "Fri" => "Ven.", "Sat" => "Sam.", "Sun" => "Dim."
+  }.freeze
+
+  def mois_en_francais(date)
+    MOIS_FR[date.strftime("%b")].upcase
+  end
+
+  def jour_en_francais(date)
+    JOURS_FR[date.strftime("%a")].upcase
+  end
 end
