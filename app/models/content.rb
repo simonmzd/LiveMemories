72d3c96e4fa463d200ec9bd0722d5443b9aa5723
Quentin Sainton <sainton.quentin86@gmail.com>
2025-03-24 15:16:57 +0100

    video

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,6 +3,7 @@ class Content < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
   validates :title, presence: true
-  validates :description, presence: true
+  # validates :image, presence: true
   has_one_attached :image
+  has_one_attached :video
 end
